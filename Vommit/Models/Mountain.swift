3dd2b7ead54ea35ba1ec352b66f9f66132075ff6
Vino Arsena <116153073+VinoArsena@users.noreply.github.com>
2026-05-01 16:45:27 +0700

    mod: modified mountain model

--- Vommit/Models/Mountain.swift
+++ Vommit/Models/Mountain.swift
@@ -1,28 +1,14 @@
 import Foundation
 
-struct Mountain {
-    let mountainId: UUID
-    let mountainName: String
-    var image: String
-    var grade: Int
-    var duration: Int
-    var elevation: Int
-    var distance: Double
-    var estimation: Range<Int>
-    var overview: String
-    var vo2max: Double
-    
-    
-    init(mountainId: UUID = UUID(), mountainName: String) {
-        self.mountainId = mountainId
-        self.mountainName = mountainName
-        self.image = ""
-        self.grade = 0
-        self.duration = 0
-        self.elevation = 0
-        self.distance = 0
-        self.estimation = 0..<0
-        self.overview = ""
-        self.vo2max = 0
-    }
+struct Mountain: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageUrl: String
+    let grade: Int
+    let duration: Int
+    let elevation: Int
+    let distance: Double
+    let estimation: Range<Int>
+    let overview: String
+    let vo2max: Double
 }
