9906ace376ae6d4037ce50edb1390da287ee9119
angmariamelani <angmariamelani@gmail.com>
2026-05-02 16:45:20 +0700

    Update DatabaseManager.swift
    feat: add mountain database

--- Vommit/Services/DatabaseManager.swift
+++ Vommit/Services/DatabaseManager.swift
@@ -1,1 +1,61 @@
+import Foundation
 
+struct DataDatabaseManager {
+    static let mountains: [Mountain] = [
+        Mountain(
+            name: "Rinjani",
+            imageUrl: "https://d18sx48t16nre5.cloudfront.net/webp_xl_9a4e03f5b7b3ff53050a863be365b8ff.webp",
+            grade: 4,
+            duration: 10,
+            elevation: 3726,
+            distance: 35,
+            estimation: 3..<4,
+            overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            vo2max: 15
+        ),
+        Mountain(
+            name: "Rinjani",
+            imageUrl: "https://d18sx48t16nre5.cloudfront.net/webp_xl_9a4e03f5b7b3ff53050a863be365b8ff.webp",
+            grade: 4,
+            duration: 10,
+            elevation: 3726,
+            distance: 35,
+            estimation: 3..<4,
+            overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            vo2max: 15
+        ),
+        Mountain(
+            name: "Rinjani",
+            imageUrl: "https://d18sx48t16nre5.cloudfront.net/webp_xl_9a4e03f5b7b3ff53050a863be365b8ff.webp",
+            grade: 4,
+            duration: 10,
+            elevation: 3726,
+            distance: 35,
+            estimation: 3..<4,
+            overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            vo2max: 15
+            ),
+        Mountain(
+            name: "Rinjani",
+            imageUrl: "https://d18sx48t16nre5.cloudfront.net/webp_xl_9a4e03f5b7b3ff53050a863be365b8ff.webp",
+            grade: 4,
+            duration: 10,
+            elevation: 3726,
+            distance: 35,
+            estimation: 3..<4,
+            overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            vo2max: 15
+            ),
+        Mountain(
+            name: "Rinjani",
+            imageUrl: "https://d18sx48t16nre5.cloudfront.net/webp_xl_9a4e03f5b7b3ff53050a863be365b8ff.webp",
+            grade: 4,
+            duration: 10,
+            elevation: 3726,
+            distance: 35,
+            estimation: 3..<4,
+            overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            vo2max: 15
+        ),
+    ]
+}
