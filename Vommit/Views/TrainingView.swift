f48df5881f7033cfb5da09a73507b3dc63b8d166
Vino Arsena <116153073+VinoArsena@users.noreply.github.com>
2026-05-02 08:11:05 +0700

    mod: set background color for all views

--- Vommit/Views/TrainingView.swift
+++ Vommit/Views/TrainingView.swift
@@ -1,1 +1,15 @@
+import SwiftUI
+
+struct TrainingView: View {
+    
+    var body: some View {
+        Text("Text")
+    }
+}
+
+#Preview {
+    TrainingView()
+        .preferredColorScheme(.dark)
+        .background(Color("Background").ignoresSafeArea())
+}
 
